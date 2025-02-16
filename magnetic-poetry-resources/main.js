8b7e938b05cbd2d958b061e0b79c27cb37e832d6
Levi Fritsch <levijfritsch@gmail.com>
2025-02-16 13:45:17 -0600

    draggable words

--- magnetic-poetry-resources/main.js
+++ magnetic-poetry-resources/main.js
@@ -1,14 +1,24 @@
 
 let wordspace = document.getElementById("wordspace");
+let spaceWidth = wordspace.scrollWidth;
+let spaceHeight = wordspace.scrollHeight;
+
 
 function createWordElement(word) {
 
     let el = document.createElement("span");
-    el.classList.add("word-el");
     el.innerText = word;
+    el.classList.add("word-el");
+    el.style.left = Math.random()*(spaceWidth - word.length*12) + "px";
+    el.style.top = Math.random()*(spaceHeight - 30) + "px";
+
+    dragElement(el);
+    
     wordspace.appendChild(el);
 }
 
 for (let i = 0; i < words.length; i++) {
     createWordElement(words[i]);
 }
+
+
